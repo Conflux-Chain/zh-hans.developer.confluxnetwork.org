1541940782ca9fc2c82480954ab03bc1244fe9f2
yqrashawn <namy.19@gmail.com>
2020-06-15 17:26:50 +0800

    docs: add zh-cn trans of overview.md (#1)

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,18 @@
 module.exports = {
   docs: {
     'zh-cn': [
+      {
+        type: "category",
+        label: "Learn About Conflux",
+        items: [
+          { type: "doc", id: "conflux_overview", folder: "introduction" },
+          {
+            type: "category",
+            label: "Technical Papers",
+            items: [],
+          },
+        ],
+      },
     ],
   },
 };

7293a602d9a30cb47f7ee08634df0cce57ea3ba5
yqrashawn <namy.19@gmail.com>
2020-06-18 14:55:50 +0800

    chore: use conflux-docusaurus-config

--- sidebars.js
+++ sidebars.js
@@ -1,18 +1,16 @@
 module.exports = {
-  docs: {
-    'zh-cn': [
-      {
-        type: "category",
-        label: "Learn About Conflux",
-        items: [
-          { type: "doc", id: "conflux_overview", folder: "introduction" },
-          {
-            type: "category",
-            label: "Technical Papers",
-            items: [],
-          },
-        ],
-      },
-    ],
-  },
+  docs: [
+    {
+      type: "category",
+      label: "Learn About Conflux",
+      items: [
+        { type: "doc", id: "conflux_overview", folder: "introduction" },
+        {
+          type: "category",
+          label: "Technical Papers",
+          items: [],
+        },
+      ],
+    },
+  ],
 };
